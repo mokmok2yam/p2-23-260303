fe828c7761a67da02af5c7e262029d141df2f4a3
HanJeongmok <wjdahe1845@gmail.com>
2026-03-04 14:40:25 +0900

    19강 수정구현

--- src/main/java/com/back/domain/wiseSaying/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/WiseSayingController.java
@@ -15,21 +15,18 @@ import java.util.stream.Collectors;
 @Controller
 public class WiseSayingController {
 
-    private final List<WiseSaying> wiseSayings=new ArrayList<>(){{
-        add(new WiseSaying(1,"1","1"));
-        add(new WiseSaying(2,"2","2"));
-        add(new WiseSaying(3,"3","3"));
-        add(new WiseSaying(4,"4","4"));
-        add(new WiseSaying(5,"5","5"));
+    private int lastId = 5;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>() {{
+        add(new WiseSaying(1, "삶이 있는 한 희망은 있다.", "키케로"));
+        add(new WiseSaying(2, "하루에 3시간을 걸으면 7년 후에 지구를 한 바퀴 돌 수 있다.", "사무엘 존슨"));
+        add(new WiseSaying(3, "언제나 현재에 집중할수 있다면 행복할 것이다.", "파울로 코엘료"));
+        add(new WiseSaying(4, "신은 용기있는 자를 결코 버리지 않는다.", "켄러"));
+        add(new WiseSaying(5, "피할 수 없으면 즐겨라.", "로버트 엘리엇"));
     }};
-    int lastId =5;
+
     @GetMapping("/write")
     @ResponseBody
-    public String write(
-            @RequestParam String content,
-            @RequestParam String author
-    ) {
-
+    public String write(@RequestParam String content, @RequestParam String author) {
         if(content == null || content.trim().length() == 0) {
             throw new RuntimeException("명언을 입력해주세요.");
         }
@@ -43,10 +40,12 @@ public class WiseSayingController {
 
         return "%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId());
     }
+
     @GetMapping("/list")
     @ResponseBody
     public String list() {
-        String wiseSayingList = wiseSayings.stream()
+
+        String wiseSayingsList = wiseSayings.stream()
                 .map(w -> "<li>%s / %s / %s</li>".formatted(w.getId(), w.getContent(), w.getAuthor()))
                 .collect(Collectors.joining("\n"));
 
@@ -54,23 +53,45 @@ public class WiseSayingController {
                 <ul>
                 %s
                 </ul>
-                """.formatted(wiseSayingList);
+                """.formatted(wiseSayingsList);
     }
-    @GetMapping("/delete/{id}")
+
+
+    @GetMapping("/delete/{id}") // delete/1, delete/2
     @ResponseBody
     public String delete(
-            @PathVariable int id
+            @PathVariable int id // 1, 2
     ) {
-                Optional<WiseSaying> wiseSaying = wiseSayings.stream()
+        WiseSaying wiseSaying = findById(id);
+        wiseSayings.remove(wiseSaying);
+
+        return "%d번 명언이 삭제되었습니다".formatted(id);
+    }
+
+    @GetMapping("/modify/{id}")
+    @ResponseBody
+    public String modify(
+            @PathVariable int id,
+            @RequestParam(defaultValue = "기본값") String content,
+            @RequestParam(defaultValue = "기본값") String author
+    ) {
+
+        WiseSaying wiseSaying = findById(id);
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+
+        return "%d번 명언이 수정되었습니다.".formatted(wiseSaying.getId());
+    }
+
+    private WiseSaying findById(int id) {
+        Optional<WiseSaying> wiseSaying = wiseSayings.stream()
                 .filter(w -> w.getId() == id)
                 .findFirst();
 
-        if(wiseSaying.isEmpty()) {
+        if (wiseSaying.isEmpty()) {
             throw new RuntimeException("%d번 명언은 존재하지 않습니다.".formatted(id));
         }
 
-        wiseSayings.remove(wiseSaying.get());
-
-        return "%d번 명언이 삭제되었습니다".formatted(id);
+        return wiseSaying.get();
     }
 }
